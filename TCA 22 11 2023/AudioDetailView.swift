dcac6631d670f65705730e510c86c06c1d1d4800
alexslkgit <alexslkmain@gmail.com>
2023-12-08 12:39:58 +0000

    Added base functional for details

--- TCA 22 11 2023/AudioDetailView.swift
+++ TCA 22 11 2023/AudioDetailView.swift
@@ -13,10 +13,11 @@ import Combine
 struct AudioDetailView: View {
     
     let store: StoreOf<AudioDetailFeature>
-        
+    
     var body: some View {
         
         WithViewStore(self.store, observe: { $0 }) { viewStore in
+            
             ZStack {
                 Color(red: 255 / 255.0, green: 248 / 255.0, blue: 243 / 255.0)
                     .edgesIgnoringSafeArea(.all)
@@ -32,60 +33,69 @@ struct AudioDetailView: View {
                     Text(viewStore.currentTitle)
                         .font(.callout)
                         .padding(.vertical, 2)
-//                    
-//                    HStack {
-//                        Text(formatTime(viewModel.currentTime))
-//                        CustomSlider(value: $viewModel.currentTime, range: 0...viewModel.duration, onEditingChanged: sliderEditingChanged)
-//                        
-//                        Text(formatTime(viewModel.duration))
-//                    }
-//                    .padding(.vertical, 10)
-//                    
-//                    Button("Speed \(formatSpeed(viewModel.playbackRate))") {
-//                        viewModel.changePlaybackRate()
-//                    }
-//                    .buttonStyle(SpeedButtonStyle())
-//                    .padding(.bottom, 20)
-//                    
-//                    HStack(spacing: 20) {
-//                        Button(action: viewModel.previousTrack) {
-//                            Image(systemName: "backward.end.fill")
-//                        }
-//                        .foregroundColor(viewModel.hasPreviousTrack ? .black : .gray)
-//                        .disabled(!viewModel.hasPreviousTrack)
-//                        
-//                        Button(action: { viewModel.rewindBy(seconds: -5) }) {
-//                            Image(systemName: "gobackward.5")
-//                        }
-//                        .foregroundColor(.black)
-//                        
-//                        Button(action: viewModel.playPause) {
-//                            Image(systemName: viewModel.isPlaying ? "pause.fill" : "play.fill")
-//                        }
-//                        .foregroundColor(.black)
-//                        
-//                        Button(action: { viewModel.rewindBy(seconds: 10) }) {
-//                            Image(systemName: "goforward.10")
-//                        }
-//                        .foregroundColor(.black)
-//                        
-//                        Button(action: viewModel.nextTrack) {
-//                            Image(systemName: "forward.end.fill")
-//                        }
-//                        .foregroundColor(viewModel.hasNextTrack ? .black : .gray)
-//                        .disabled(!viewModel.hasNextTrack)
-//                    }
-//                    .font(.title)
-//                    .padding(.bottom, 20)
-//                    Spacer(minLength: UIScreen.main.bounds.height * 0.1)
-//                }
-//                .padding()
-//                .onAppear {
-//                    self.viewModel.initializeSubscribers()
-//                }
-//                .onDisappear {
-//                    self.viewModel.stop()
+                    
+                    //                    HStack {
+                    //                        Text(formatTime(viewStore.currentTime))
+                    //                        CustomSlider(value: viewStore.state.currentTime, range: 0...viewStore.state.duration, onEditingChanged: {
+                    //                            if !sliderEditingChanged { viewStore.seekTo(viewStore.currentTime) }
+                    //                        })
+                    //
+                    //                        Text(formatTime(viewStore.duration))
+                    //                    }
+                    //                    .padding(.vertical, 10)
+                    
+                    Button("Speed \(formatSpeed(viewStore.playbackRate))") {
+                        viewStore.send(.changePlaybackRate)
+                    }
+                    .buttonStyle(SpeedButtonStyle())
+                    .padding(.bottom, 20)
+                    
+                    HStack(spacing: 20) {
+                        Button(action: {
+                            viewStore.send(.previousTrack)
+                        }) {
+                            Image(systemName: "backward.end.fill")
+                        }
+                        .foregroundColor(viewStore.state.hasPreviousTrack ? .black : .gray)
+                        .disabled(!viewStore.state.hasPreviousTrack)
+                        
+                        Button(action: {
+                            viewStore.send(.rewind(-5))
+                        }) {
+                            Image(systemName: "gobackward.5")
+                        }
+                        .foregroundColor(.black)
+                        
+                        Button(action: {
+                            viewStore.send(.playPause)
+                        }) {
+                            Image(systemName: viewStore.state.isPlaying ? "pause.fill" : "play.fill")
+                        }
+                        .foregroundColor(.black)
+                        
+                        Button(action: {
+                            viewStore.send(.rewind(10))
+                        }) {
+                            Image(systemName: "goforward.10")
+                        }
+                        .foregroundColor(.black)
+                        
+                        Button(action: {
+                            viewStore.send(.nextTrack)
+                        }) {
+                            Image(systemName: "forward.end.fill")
+                        }
+                        .foregroundColor(viewStore.state.hasNextTrack ? .black : .gray)
+                        .disabled(!viewStore.state.hasNextTrack)
+                    }.onAppear {
+                        viewStore.send(.onAppear)
+                    }
+                    .font(.title)
+                    .padding(.bottom, 20)
+                    Spacer(minLength: UIScreen.main.bounds.height * 0.1)
                 }
+                .padding()
+                
             }
         }
     }
@@ -94,24 +104,30 @@ struct AudioDetailView: View {
     //    self.audioPlayer = AVPlayer(url: url)
     //    self.audioPlayer?.play()
     
+    //    .onAppear {
+    //        self.viewStore.initializeSubscribers()
+    //    }
+    //    .onDisappear {
+    //        self.viewStore.stop()
+    //    }
     
-//    private func sliderEditingChanged(editingStarted: Bool) {
-//        if !editingStarted { viewModel.seekTo(viewModel.currentTime) }
-//    }
-//    
-//    private func formatTime(_ seconds: Double) -> String {
-//        let time = Int(seconds)
-//        let minutes = time / 60 % 60
-//        let seconds = time % 60
-//        return String(format: "%02i:%02i", minutes, seconds)
-//    }
-//    
-//    private func formatSpeed(_ rate: Float) -> String {
-//        if rate.truncatingRemainder(dividingBy: 1) == 0 {
-//            return String(format: "%.0f", rate)
-//        } else {
-//            return String(format: "%.1f", rate)
-//        }
-//    }
+    private func sliderEditingChanged(editingStarted: Bool) {
+        
+    }
+    
+    private func formatTime(_ seconds: Double) -> String {
+        let time = Int(seconds)
+        let minutes = time / 60 % 60
+        let seconds = time % 60
+        return String(format: "%02i:%02i", minutes, seconds)
+    }
+    
+    private func formatSpeed(_ rate: Float) -> String {
+        if rate.truncatingRemainder(dividingBy: 1) == 0 {
+            return String(format: "%.0f", rate)
+        } else {
+            return String(format: "%.1f", rate)
+        }
+    }
     
 }
